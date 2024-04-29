663f1b7b585fe5f9bfc1b99ed5922025785e93b5
KugoKento <618953kk@gmail.com>
2024-04-29 16:50:17 +0900

    TagMo repositry 作成

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Start TagMo!</Text>
+      <Text>Start TagMo!!</Text>
       <StatusBar style="auto" />
     </View>
   );
